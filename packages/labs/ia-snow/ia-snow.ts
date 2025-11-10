e52ab13ca0e61eb2f7babcae1a19a83ef53a80fe
Jason Buckner <jason@jasonbuckner.com>
2025-11-10 13:33:31 -0800

    Rename directories to match component name

---
+++ packages/labs/ia-snow/ia-snow.ts
@@ -0,0 +1,40 @@
+import { html, LitElement } from "lit";
+import { state } from "lit/decorators/state.js";
+import { customElement } from "lit/decorators/custom-element.js";
+
+import Snowflakes from 'magic-snowflakes';
+
+import '@internetarchive/ia-components/components/ia-button/ia-button.js';
+
+
+@customElement("ia-snow")
+export class Snow extends LitElement {
+  @state()
+  private snowing = false;
+
+  private snowflakes?: Snowflakes;
+
+  render() {
+    return html`
+      <h1>Snowflakes</h1>
+      <ia-button @click=${() => {
+        this.snowing ? this.stopSnowing() : this.startSnowing();
+      }}>
+        ${this.snowing ? "Stop Snowflakes" : "Start Snowflakes"}
+      </ia-button>
+    `;
+  }
+
+  private startSnowing() {
+    if (!this.snowflakes) {
+      this.snowflakes = new Snowflakes();
+    }
+    this.snowflakes?.start();
+    this.snowing = true;
+  }
+
+  private stopSnowing() {
+    this.snowflakes?.stop();
+    this.snowing = false;
+  }
+}
