bfbb71da9880419405d088110b3dd41640afaeba
rebecca shoptaw <rebecca@archive.org>
2026-01-22 13:56:16 -0500

    WEBDEV-8109 Expand status indicator capabilities (#23)
    * Tidy up and build basic success indicator
    
    * Style success indicator
    
    * Build out error icon
    
    * Allow loading dots override
    
    * Add all new styles and props to story
    
    * Support radio input
    
    * Tidy up prop inputs
    
    * Adjust loading dots option and add to story
    
    * Improve props legibility

--- src/elements/ia-status-indicator/ia-status-indicator.ts
+++ src/elements/ia-status-indicator/ia-status-indicator.ts
@@ -1,6 +1,7 @@
 import { css, CSSResultGroup, html, LitElement, TemplateResult } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { msg } from '@lit/localize';
+import { choose } from 'lit/directives/choose.js';
 
 import themeStyles from '@src/themes/theme-styles';
 
@@ -12,42 +13,111 @@ export class IAStatusIndicator extends LitElement {
   /* An optional title to use for the loading state of the indicator. Will be used for screen readers. */
   @property({ type: String }) loadingTitle = msg('Loading...');
 
+  /* An optional title to use for the success state of the indicator. Will be used for screen readers. */
+  @property({ type: String }) successTitle = msg('Success');
+
+  /* An optional title to use for the error state of the indicator. Will be used for screen readers. */
+  @property({ type: String }) errorTitle = msg('Error');
+
+  /* Which type of loading indicator to display */
+  @property({ type: String }) loadingStyle: 'ring' | 'ring-dots' = 'ring-dots';
+
+  /* The state of the indicator that should be shown */
+  @property({ type: String }) mode: 'loading' | 'success' | 'error' = 'loading';
+
   render(): TemplateResult {
-    return this.circularLoadingIndicatorTemplate;
+    return html`${choose(this.mode, [
+      ['loading', () => this.loadingIndicatorTemplate],
+      ['success', () => this.successIndicatorTemplate],
+      ['error', () => this.errorIndicatorTemplate],
+    ])}`;
   }
 
-  /** A circular animated loading indicator */
-  private get circularLoadingIndicatorTemplate(): TemplateResult {
+  /** A circular loading indicator to render when processing */
+  private get loadingIndicatorTemplate(): TemplateResult {
     return html`
-      <div class="circular-loading-indicator">
-        <svg
-          viewBox="0 0 120 120"
-          preserveAspectRatio="none"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          role="status"
-        >
-          <title>${this.loadingTitle}</title>
-          <g stroke="none" stroke-width="1" fill-rule="evenodd">
-            <path
-              id="activity-ring"
-              class="loading-ring"
-              d="M60,10 C69.8019971,10 78.9452178,12.8205573 86.6623125,17.6943223 L76.4086287,27.9484118 C71.4880919,25.4243078 65.9103784,24 60,24 C40.117749,24 24,40.117749 24,60 C24,79.882251 40.117749,96 60,96 C79.882251,96 96,79.882251 96,60 C96,53.3014663 94.1704984,47.0302355 90.9839104,41.6587228 L101.110332,31.5326452 C106.715332,39.6116982 110,49.4222615 110,60 C110,87.6142375 87.6142375,110 60,110 C32.3857625,110 10,87.6142375 10,60 C10,32.3857625 32.3857625,10 60,10 Z"
-            ></path>
-            <g
-              id="activity-dots"
-              class="loading-dots"
-              transform="translate(40.000000, 55.000000)"
-            >
-              <circle id="left-dot" cx="5" cy="5" r="5"></circle>
-              <circle id="middle-dot" cx="20" cy="5" r="5"></circle>
-              <circle id="right-dot" cx="35" cy="5" r="5"></circle>
-            </g>
+      <svg
+        class="loading-indicator"
+        viewBox="0 0 120 120"
+        preserveAspectRatio="none"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        role="status"
+      >
+        <title>${this.loadingTitle}</title>
+        <g stroke="none" stroke-width="1" fill-rule="evenodd">
+          <path
+            class="loading-ring"
+            d="M60,10 C69.8019971,10 78.9452178,12.8205573 86.6623125,17.6943223 L76.4086287,27.9484118 C71.4880919,25.4243078 65.9103784,24 60,24 C40.117749,24 24,40.117749 24,60 C24,79.882251 40.117749,96 60,96 C79.882251,96 96,79.882251 96,60 C96,53.3014663 94.1704984,47.0302355 90.9839104,41.6587228 L101.110332,31.5326452 C106.715332,39.6116982 110,49.4222615 110,60 C110,87.6142375 87.6142375,110 60,110 C32.3857625,110 10,87.6142375 10,60 C10,32.3857625 32.3857625,10 60,10 Z"
+          ></path>
+          <g
+            class="loading-dots ${!this.shouldShowLoadingDots ? 'hidden' : ''}"
+            transform="translate(40.000000, 55.000000)"
+          >
+            <circle id="left-dot" cx="5" cy="5" r="5"></circle>
+            <circle id="middle-dot" cx="20" cy="5" r="5"></circle>
+            <circle id="right-dot" cx="35" cy="5" r="5"></circle>
           </g>
-        </svg>
-      </div>
+        </g>
+      </svg>
     `;
+  }
+
+  /** A check mark to render on success */
+  private get successIndicatorTemplate(): TemplateResult {
+    return html`
+      <svg
+        class="success-indicator"
+        viewBox="0 0 120 120"
+        preserveAspectRatio="none"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        role="status"
+      >
+        <title>${this.successTitle}</title>
+        <g stroke="none" stroke-width="1" fill-rule="evenodd">
+          <path
+            class="success-icon"
+            d="M60,10 C70.5816709,10 80.3955961,13.2871104 88.4763646,18.8959201 L78.3502633,29.0214223 C72.9767592,25.8315427 66.7022695,24 60,24 C40.117749,24 24,40.117749 24,60 C24,79.882251 40.117749,96 60,96 C79.882251,96 96,79.882251 96,60 L95.995,59.46 L108.327675,47.128668 C109.350926,50.9806166 109.925886,55.015198 109.993301,59.1731586 L110,60 C110,87.6142375 87.6142375,110 60,110 C32.3857625,110 10,87.6142375 10,60 C10,32.3857625 32.3857625,10 60,10 Z"
+          ></path>
+          <polygon
+            class="success-icon"
+            transform="translate(75.000000, 41.500000) rotate(44.000000) translate(-75.000000, -41.500000) "
+            points="96 85 54 85 54 65 76 64.999 76 -2 96 -2"
+          ></polygon>
+        </g>
+      </svg>
+    `;
+  }
+
+  /** An "!" to render on error */
+  private get errorIndicatorTemplate(): TemplateResult {
+    return html`
+      <svg
+        class="error-indicator"
+        viewBox="0 0 120 120"
+        preserveAspectRatio="none"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        role="status"
+      >
+        <title>${this.errorTitle}</title>
+        <path
+          class="error-icon"
+          d="m56.4612493 8.80450354 41.8901185 75.94632926c1.7706782 2.8433173 2.1150372 5.2623412 1.0330766 7.2570716-1.0819604 1.9947304-3.26978 2.9920956-6.5634587 2.9920956h-85.69973905c-3.29367873 0-5.46954894-.9973652-6.52761065-2.9920956-1.0580617-1.9947304-.70175345-4.4137543 1.06892476-7.2570716l41.89011844-75.12308969c1.8184757-2.84331737 3.9693609-4.37738627 6.4526556-4.60220671s4.6341799 1.03483527 6.4526556 3.77896714zm28.5387507 75.19549646-35.037482-62-34.962518 62zm-31-34.7484359v-10.2515641h-8v10.2515641l2.089172 14.7484359h3.8184713zm-8 19.7484359v8h8v-8z"
+        />
+      </svg>
+    `;
+  }
+
+  /* Whether to include the dots in the loading indicator */
+  private get shouldShowLoadingDots(): boolean {
+    if (this.loadingStyle === 'ring') return false;
+
+    return true;
   }
 
   static get styles(): CSSResultGroup {
@@ -55,30 +125,44 @@ export class IAStatusIndicator extends LitElement {
       themeStyles,
       css`
         :host {
+          --indicator-width--: var(--icon-width);
+
+          /* Loading */
           --loading-ring-color--: var(--primary-text-color);
           --loading-dot-color--: var(--primary-text-color);
-          --loading-indicator-width--: var(--icon-width);
+
+          /* Success */
+          --success-icon-color--: var(--color-success);
+
+          /* Error */
+          --error-icon-color--: var(--color-danger);
 
           display: inline-block;
-          width: var(--loading-indicator-width--);
+          width: var(--indicator-width--);
         }
 
-        #activity-ring {
-          fill: var(--loading-ring-color--);
+        .success-icon {
+          fill: var(--success-icon-color--);
         }
 
-        #activity-dots {
-          fill: var(--loading-dot-color--);
+        .error-icon {
+          fill: var(--error-icon-color--);
         }
 
         .loading-ring {
+          fill: var(--loading-ring-color--);
           animation: rotate 1.3s infinite linear;
           transform-origin: 50px 50px;
           transform-box: fill-box;
         }
 
         .loading-dots {
+          fill: var(--loading-dot-color--);
           transition: opacity 0.25s ease-out;
+        }
+
+        .loading-dots.hidden {
+          display: none;
         }
 
         .loading-dots > * {
