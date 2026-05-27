18506f04602060a4818e8aec5472dfd5cac5b882
rebecca shoptaw <rebecca@rebeccashoptaw.dev>
2026-05-27 15:36:48 -0400

    Add Offshoot button to elements

--- src/elements/ia-button/ia-button.test.ts
+++ src/elements/ia-button/ia-button.test.ts
@@ -2,13 +2,106 @@ import { fixture } from '@open-wc/testing-helpers';
 import { describe, expect, test } from 'vitest';
 import { html } from 'lit';
 
-import type { IAButton } from './ia-button';
+import { IAButton } from './ia-button';
 import './ia-button';
 
 describe('IA button', () => {
   test('renders a basic button', async () => {
-    const el = await fixture<IAButton>(html`<ia-button>Click me</ia-button>`);
+    const el = await fixture<IAButton>(html`<ia-button>Submit</ia-button>`);
+
     const button = el.shadowRoot?.querySelector('button');
-    expect(button).toBeDefined();
+    expect(button).to.exist;
+    expect(button?.disabled).to.equal(false);
+  });
+
+  test('displays slotted text within button', async () => {
+    const el = await fixture<IAButton>(
+      html`<ia-button><span class="foo">Submit</span></ia-button>`,
+    );
+
+    const buttonText = el.shadowRoot
+      ?.querySelector('slot')
+      ?.assignedElements()[0];
+    expect(buttonText).to.exist;
+    expect(buttonText?.innerHTML).to.contain('Submit');
+  });
+
+  test('shows a loading state if requested', async () => {
+    const el = await fixture<IAButton>(
+      html`<ia-button ?loading=${true}></ia-button>`,
+    );
+
+    const button = el.shadowRoot?.querySelector('button');
+    expect(button?.disabled).to.equal(true);
+    expect(button?.innerText).to.equal('');
+
+    const loadingIndicator = button?.querySelector('.loading-indicator');
+    expect(loadingIndicator).to.exist;
+  });
+
+  test('shows text next to the loading indicator if requested', async () => {
+    const el = await fixture<IAButton>(html`
+      <ia-button ?loading=${true} .loadingText=${'Loading...'}>
+        Submit
+      </ia-button>
+    `);
+
+    const button = el.shadowRoot?.querySelector('button');
+    expect(button?.disabled).to.equal(true);
+    expect(button?.innerText).to.equal('Loading...');
+
+    const loadingIndicator = button?.querySelector('.loading-indicator');
+    expect(loadingIndicator).to.exist;
+  });
+
+  test('adds a hidden light DOM submit input if type set to submit', async () => {
+    const el = await fixture<IAButton>(html`
+      <ia-button type="submit"> Submit </ia-button>
+    `);
+
+    await el.updateComplete;
+
+    const hiddenInput = el.querySelector('input[type="submit"]');
+    expect(hiddenInput).to.exist;
+  });
+
+  test('does add a hidden light DOM reset input if type set to reset', async () => {
+    const el = await fixture<IAButton>(html`
+      <ia-button .type=${'reset'}>Clear</ia-button>
+    `);
+
+    await el.updateComplete;
+
+    const hiddenInput = el.querySelector('input[type="reset"]');
+    expect(hiddenInput).to.exist;
+  });
+
+  test('does not add a hidden light DOM input if type not set', async () => {
+    const el = await fixture<IAButton>(html`<ia-button>Submit</ia-button>`);
+
+    await el.updateComplete;
+
+    const hiddenInput = el.querySelector('input[type="submit"]');
+    expect(hiddenInput).not.to.exist;
+  });
+
+  test('does not add a hidden light DOM input if type set to button', async () => {
+    const el = await fixture<IAButton>(
+      html`<ia-button .type=${'button'}>Submit</ia-button>`,
+    );
+
+    await el.updateComplete;
+
+    const hiddenInput = el.querySelector('input[type="submit"]');
+    expect(hiddenInput).not.to.exist;
+  });
+
+  test('disables the button if requested', async () => {
+    const el = await fixture<IAButton>(
+      html`<ia-button ?disabled=${true}>Submit</ia-button>`,
+    );
+
+    const button = el.shadowRoot?.querySelector('button');
+    expect(button?.disabled).to.equal(true);
   });
 });
--- src/elements/ia-button/ia-button.ts
+++ src/elements/ia-button/ia-button.ts
@@ -1,36 +1,290 @@
-import { css, html, LitElement, type CSSResultGroup } from 'lit';
-import { customElement } from 'lit/decorators/custom-element.js';
+import {
+  html,
+  LitElement,
+  TemplateResult,
+  CSSResultGroup,
+  css,
+  PropertyValues,
+  render,
+} from 'lit';
+import { msg } from '@lit/localize';
+import { property, customElement } from 'lit/decorators.js';
 
-import themeStyles from '@src/themes/theme-styles';
+import '../ia-status-indicator/ia-status-indicator';
 
 /**
- * A button element to demo the elements library
+ * Renders a standardized button built off the ia-button styling and the activity indicator, with the option to show
+ * a loading indicator instead of the text or to customize the text, and the ability to pass in any
+ * function to run on click.
+ *
+ * Designed to behave exactly like a native type="submit" or type="reset" button if either type is supplied,
+ * i.e. for an <ia-button type="submit" @click=${doSideEffects}></ia-button>,
+ * whether the form is submitted via enter or by clicking the button directly, the button will always
+ * run doSideEffects() and then submit the form.
  */
 @customElement('ia-button')
 export class IAButton extends LitElement {
-  render() {
+  /* Whether to show a loading indicator instead of the button */
+  @property({ type: Boolean }) loading: boolean = false;
+
+  /* Whether the button should be disabled, regardless of submission status */
+  @property({ type: Boolean }) disabled: boolean = false;
+
+  /* Optional text to include next to the loading indicator */
+  @property({ type: String }) loadingText: string = '';
+
+  /* Type of the button - defaults to 'button' to prevent form submission */
+  @property({ type: String, reflect: true }) type:
+    | 'button'
+    | 'submit'
+    | 'reset' = 'button';
+
+  render(): TemplateResult {
     return html`
-      <button>
-        <slot></slot>
+      <button ?disabled=${this.loading || this.disabled}>
+        ${this.loading ? this.loadingStateTemplate : html`<slot></slot>`}
       </button>
+      <slot name="hidden-btn"></slot>
     `;
   }
 
-  static get styles(): CSSResultGroup {
-    return [
-      themeStyles,
-      css`
-        :host {
-          --primary-background-color--: var(--primary-cta-fill);
-          --primary-text-color--: var(--primary-cta-text-color);
-        }
+  protected willUpdate(changed: PropertyValues): void {
+    if (changed.has('type')) {
+      this.setButtonTypeEmulation();
+    }
+  }
 
-        button {
-          padding: 8px 16px;
-          background-color: var(--primary-background-color--);
-          color: var(--primary-text-color--);
-        }
-      `,
-    ];
+  /* Content to render while button is loading */
+  private get loadingStateTemplate(): TemplateResult {
+    return html`
+      <span class="loading-indicator" alt="Loading indicator">
+        <ia-status-indicator mode="loading"></ia-status-indicator> ${msg(
+          this.loadingText,
+        )}
+      </span>
+    `;
+  }
+
+  /** Sets up or removes button type emulation as needed */
+  private setButtonTypeEmulation(): void {
+    this.removeExistingTypeEmulation();
+    this.emulateButtonTypeBehavior();
+  }
+
+  /**
+   * Removes all existing button behavior emulation
+   * to ensure any new button type takes precendence.
+   */
+  private removeExistingTypeEmulation(): void {
+    this.removeEventListener('click', this.handleComponentClick);
+    this.querySelector('.hidden-button')?.remove();
+  }
+
+  /**
+   * If the button type set to "submit" or "reset",
+   * adds extra behavior to ensure
+   * button emulates native button behavior.
+   * */
+  private emulateButtonTypeBehavior(): void {
+    if (this.type === 'button') return;
+
+    this.addHiddenButton();
+    this.addEventListener('click', this.handleComponentClick);
+  }
+
+  /**
+   * Triggers form actions (submit/reset) on click, if desired
+   * and if actions not already in progress
+   */
+  private handleComponentClick(e: Event): void {
+    if (this.type === 'button') return;
+
+    // No need to submit/reset the form if it's already submitting/resetting
+    const formActionsInProgress =
+      e instanceof CustomEvent && e.detail.formActionsInProgress;
+    if (formActionsInProgress) return;
+
+    // Request form submission/reset by clicking the hidden native submit/reset button we added earlier
+    const hiddenButton = this.querySelector(
+      'input.hidden-button',
+    ) as HTMLInputElement;
+    hiddenButton.dispatchEvent(new PointerEvent('click'));
+  }
+
+  /**
+   * Adds a hidden button to the light DOM to
+   * emulate native reset/submit button behavior.
+   *
+   * Used for:
+   * - Submitting/resetting the form (on behalf of handleComponentClick)
+   * - Activating any component click events if form is submitted with the enter key
+   * (via handleFormActions)
+   */
+  private addHiddenButton(): void {
+    if (this.type === 'button') return;
+
+    render(
+      html`<input
+        type=${this.type}
+        class="hidden-button"
+        style="display:none"
+        slot="hidden-button"
+        @click=${(e: Event) => this.handleFormActions(e)}
+      />`,
+      this,
+    );
+  }
+
+  /** Triggers the component's click event manually to prevent extra submission/reset */
+  private handleFormActions(e: Event): void {
+    /**
+     * Prevents the event from bubbling up to the ia-button component itself.
+     * This way, if the click on this button is triggered by a click on the component,
+     * we don't accidentally bubble up a second click and end up in an infinite loop.
+     *
+     * And if the click was triggered by form submission (via the enter key), we can hijack the click
+     * event to pass that info to the component, so it knows not to
+     * re-attempt submission.
+     */
+    e.stopPropagation();
+
+    // Detect if click comes from the parent component,
+    // in which case there's no need to trigger a second click on it
+    const triggeredByComponentClick = !e.isTrusted;
+    if (triggeredByComponentClick) return;
+
+    // Trigger parent component click events manually,
+    // so it knows not to try to re-submit/re-reset the form
+    this.dispatchEvent(
+      new CustomEvent('click', { detail: { formActionsInProgress: true } }),
+    );
+  }
+
+  static get styles(): CSSResultGroup {
+    return css`
+      :host {
+        display: inline-block; /* keeps host sized to button */
+      }
+
+      button {
+        height: var(--height, 3.5rem);
+        min-height: 3rem;
+        cursor: pointer;
+        color: var(--textColor, #fff);
+        background: var(--backgroundColor, initial);
+        line-height: normal;
+        border-radius: 0.4rem;
+        font-size: var(--fontSize, 1.4rem);
+        font-family: inherit;
+        border: var(--borderWidth, 1px) solid var(--borderColor, transparent);
+        white-space: nowrap;
+        appearance: auto;
+        box-sizing: border-box;
+        display: flex;
+        align-items: center;
+        transition: var(--transition, all 0.1s ease 0s);
+        vertical-align: middle;
+        padding: var(--padding, 0 3rem);
+        outline-color: var(--textColor, #fff);
+        outline-offset: -4px;
+        user-select: none;
+        text-decoration: none;
+        width: var(--width, fit-content);
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        -o-user-select: none;
+      }
+
+      button:disabled {
+        cursor: not-allowed;
+        background-color: var(--primaryDisableCTAFill, #767676);
+        border: 1px solid var(--secondaryCTABorder, #999);
+        opacity: 0.5;
+      }
+
+      button:enabled:hover {
+        opacity: 0.9;
+      }
+
+      button:focus-visible {
+        opacity: 0.8;
+        outline-style: double;
+      }
+
+      button:active {
+        opacity: 0.7;
+      }
+
+      :host(.primary) button:enabled {
+        background-color: var(--primaryCTAFill, #194880);
+        border-color: var(--primaryCTABorder, #c5d1df);
+      }
+
+      :host(.danger) button:enabled {
+        background-color: var(--primaryErrorCTAFill, #d9534f);
+        border-color: var(--primaryErrorCTABorder, #d43f3a);
+      }
+
+      :host(.dark) button:enabled {
+        background-color: var(--secondaryCTAFill, #333);
+        border-color: var(--primaryCTABorder, #979797);
+      }
+
+      :host(.warning) button:enabled {
+        background-color: var(--warningCTAFill, #ee8950);
+        border-color: var(--warningCTABorder, #ec7939);
+      }
+
+      :host(.custom-active-color) button:enabled:is(:hover, :focus, :active),
+      :host(.custom-active-color.active) button:enabled {
+        color: var(--customActiveForegroundColor, #194880);
+        background-color: var(--customActiveBackgroundColor, #c5d1df);
+        border-color: var(
+          --customActiveBorderColor,
+          --customActiveForegroundColor,
+          #194880
+        );
+      }
+
+      :host(.transparent) button:enabled {
+        background-color: transparent;
+      }
+
+      :host(.fit-content) button {
+        padding: 0;
+        height: fit-content;
+      }
+
+      :host(.link) button {
+        margin: 0;
+        border: 0;
+        appearance: none;
+        background: none;
+        color: var(--ia-theme-link-color, #4b64ff);
+        text-decoration: none;
+        cursor: pointer;
+        padding: 0;
+      }
+
+      :host(.link.danger-link) button {
+        color: var(--ia-theme-danger-link-color, #c9302c);
+      }
+
+      :host(.link) button:hover {
+        text-decoration: underline;
+      }
+
+      .loading-indicator {
+        display: flex;
+        flex-direction: row;
+        gap: 0.5rem;
+        align-items: center;
+      }
+
+      ia-status-indicator {
+        --primary-text-color: var(--textColor, #fff);
+      }
+    `;
   }
 }
