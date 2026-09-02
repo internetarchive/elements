e3eecd25a2cbf0213201130b24b0744067d537e6
jbuckner <jbuckner@users.noreply.github.com>
2026-09-02 23:17:26 +0000

    Deploy preview for PR 80 🛫

---
+++ pr/pr-80/assets/ia-snow-story-nkTECm-w.js
@@ -0,0 +1,62 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./ia-button-DvWmnm2b.js","./index-B8SN2BEM.js","./index-9C7dCk-X.css","./runtime-CCgtQBty.js","./story-template-DocKHMLt.js","./ia-status-indicator-DKYeldw6.js"])))=>i.map(i=>d[i]);
+import{e as y,c as S,r as g,n as b,t as m,i as v,b as p,_ as w,a as _}from"./index-B8SN2BEM.js";import{e as u}from"./query-CQu6XzkY.js";import"./story-template-DocKHMLt.js";const k="data:image/svg+xml,%3csvg%20xmlns='http://www.w3.org/2000/svg'%20width='36.283'%20height='36.283'%3e%3cpath%20d='M35.531%2017.391h-3.09l.845-1.464a.748.748%200%201%200-1.297-.75l-1.276%202.214H28.61l2.515-4.354a.751.751%200%200%200-.272-1.024.75.75%200%200%200-1.024.274l-2.948%205.104h-2.023a6.751%206.751%200%200%200-2.713-4.684l1.019-1.76%205.896-.002a.75.75%200%200%200%200-1.5l-5.029.002%201.051-1.82%202.557.002a.75.75%200%200%200%200-1.5l-1.689-.002%201.545-2.676a.75.75%200%201%200-1.302-.75l-1.547%202.676-.844-1.463a.749.749%200%201%200-1.297.75l1.278%202.213-1.051%201.818-2.514-4.354a.75.75%200%200%200-1.298.75l2.946%205.104-1.016%201.758a6.692%206.692%200%200%200-2.706-.57%206.74%206.74%200%200%200-2.707.568l-1.013-1.754%202.946-5.105a.75.75%200%200%200-1.298-.75L13.56%208.697l-1.05-1.818%201.278-2.217a.749.749%200%200%200-1.298-.75l-.845%201.465-1.551-2.678a.75.75%200%200%200-1.024-.273.748.748%200%200%200-.274%201.023l1.545%202.678H8.652a.75.75%200%200%200%200%201.5h2.556l1.05%201.818H7.231a.75.75%200%200%200%200%201.5h5.894l1.017%201.762a6.755%206.755%200%200%200-2.712%204.684H9.406l-2.95-5.104a.75.75%200%201%200-1.299.75l2.516%204.354H5.569l-1.277-2.213a.75.75%200%200%200-1.298.75l.845%201.463H.75a.75.75%200%200%200%200%201.5h3.09l-.845%201.465a.747.747%200%200%200%20.275%201.022.75.75%200%200%200%20.374.103.75.75%200%200%200%20.65-.375l1.277-2.215h2.103l-2.516%204.354a.75.75%200%200%200%201.299.75l2.949-5.104h2.024a6.761%206.761%200%200%200%202.712%204.685l-1.017%201.762H7.232a.75.75%200%200%200%200%201.5h5.026l-1.05%201.818H8.651a.75.75%200%200%200%200%201.5h1.69l-1.545%202.676a.75.75%200%200%200%201.299.75l1.546-2.676.846%201.465a.755.755%200%200%200%20.65.375.737.737%200%200%200%20.375-.103.747.747%200%200%200%20.274-1.022l-1.279-2.215%201.05-1.82%202.515%204.354a.75.75%200%200%200%201.299-.75l-2.947-5.104%201.013-1.756a6.72%206.72%200%200%200%205.415%200l1.014%201.756-2.947%205.104a.75.75%200%200%200%201.298.75l2.515-4.354%201.053%201.82-1.277%202.213a.75.75%200%200%200%201.298.75l.844-1.463%201.545%202.678c.141.24.393.375.65.375a.75.75%200%200%200%20.649-1.125l-1.548-2.678h1.689a.75.75%200%200%200%200-1.5h-2.557l-1.051-1.82%205.029.002a.75.75%200%200%200%200-1.5l-5.896-.002-1.019-1.76a6.75%206.75%200%200%200%202.711-4.685h2.023l2.947%205.104a.753.753%200%200%200%201.025.273.749.749%200%200%200%20.272-1.023l-2.515-4.354h2.104l1.279%202.215a.75.75%200%200%200%20.649.375c.127%200%20.256-.03.375-.103a.748.748%200%200%200%20.273-1.022l-.848-1.465h3.092a.75.75%200%200%200%20.003-1.5zm-12.136.75c0%20.257-.041.502-.076.75a5.223%205.223%200%200%201-1.943%203.358%205.242%205.242%200%200%201-1.291.766%205.224%205.224%200%200%201-1.949.384%205.157%205.157%200%200%201-3.239-1.15%205.22%205.22%200%200%201-1.943-3.358c-.036-.247-.076-.493-.076-.75s.04-.503.076-.75a5.22%205.22%200%200%201%201.944-3.359c.393-.312.82-.576%201.291-.765a5.219%205.219%200%200%201%201.948-.384c.69%200%201.344.142%201.948.384.471.188.898.454%201.291.765a5.222%205.222%200%200%201%201.943%203.359c.035.247.076.493.076.75z'%20fill=':color:'/%3e%3c/svg%3e",h=new WeakSet;class $ extends S{constructor(t){super(t)}update(t,[o,n]){return h.has(t)||(o(),h.add(t)),this.render(o,n)}render(t,o){return o()}}const f=y($);var I=Object.defineProperty,x=Object.getOwnPropertyDescriptor,d=(a,t,o,n)=>{for(var e=n>1?void 0:n?x(t,o):t,s=a.length-1,i;s>=0;s--)(i=a[s])&&(e=(n?i(t,o,e):i(e))||e);return n&&e&&I(t,o,e),e};let c=class extends v{constructor(){super(...arguments),this.snowing=!1}render(){return p`
+      ${this.startButtonTemplate} ${this.clearButtonTemplate}
+      <img src=${k} alt="Snowflakes icon" />
+    `}willUpdate(a){a.has("snowConfig")&&(this.snowflakes?.destroy(),this.snowflakes=void 0,this.startSnowing())}get startButtonTemplate(){return f(async()=>{await w(()=>import("./ia-button-DvWmnm2b.js"),__vite__mapDeps([0,1,2,3,4,5]),import.meta.url)},()=>p`
+        <ia-button
+          @click=${()=>{this.snowing?this.stopSnowing():this.startSnowing()}}
+        >
+          ${this.snowing?"Stop Snowflakes":"Start Snowflakes"}
+        </ia-button>
+      `)}get clearButtonTemplate(){return f(async()=>{await w(()=>import("./ia-button-DvWmnm2b.js"),__vite__mapDeps([0,1,2,3,4,5]),import.meta.url)},()=>p`
+        <ia-button
+          @click=${()=>{this.snowflakes?.destroy()}}
+        >
+          Clear Snowflakes
+        </ia-button>
+      `)}async startSnowing(){if(!this.snowflakes){const t=(await w(()=>import("./snowflakes.esm-Dz_MTlsm.js"),[],import.meta.url)).default;this.snowflakes=new t(this.snowConfig)}this.snowflakes?.start(),this.snowing=!0}stopSnowing(){this.snowflakes?.stop(),this.snowing=!1}static get styles(){return _`
+      img {
+        width: 16px;
+        filter: invert(1);
+        vertical-align: middle;
+      }
+    `}};d([b({type:Object})],c.prototype,"snowConfig",2);d([g()],c.prototype,"snowing",2);c=d([m("ia-snow")],c);var C=Object.defineProperty,O=Object.getOwnPropertyDescriptor,r=(a,t,o,n)=>{for(var e=n>1?void 0:n?O(t,o):t,s=a.length-1,i;s>=0;s--)(i=a[s])&&(e=(n?i(t,o,e):i(e))||e);return n&&e&&C(t,o,e),e};let l=class extends v{render(){return p`
+      <story-template
+        elementTag="ia-snow"
+        elementClassName="IASnow"
+        .customExampleUsage=${this.exampleUsage}
+        labs
+      >
+        <div slot="demo">
+          <ia-snow .snowConfig=${this.config}></ia-snow>
+        </div>
+
+        <div slot="settings">
+          <table>
+            <tr>
+              <td>Color</td>
+              <td><input type="color" value="#4d94b2" id="color" /></td>
+            </tr>
+            <tr>
+              <td>Count</td>
+              <td><input type="number" value="50" id="count" /></td>
+            </tr>
+            <tr>
+              <td>Wind</td>
+              <td><input type="checkbox" checked id="wind" /></td>
+            </tr>
+            <tr>
+              <td>Rotation</td>
+              <td><input type="checkbox" checked id="rotation" /></td>
+            </tr>
+          </table>
+          <button @click=${this.setupSnowflakes}>Apply</button>
+        </div>
+      </story-template>
+    `}get exampleUsage(){return this.config?`
+      <ia-snow .snowConfig=\${${this.configString}}></ia-snow>
+    `:"<ia-snow></ia-snow>"}get configString(){return JSON.stringify(this.config,null,2)}get snowflakeConfig(){return{color:this.colorInput.value,count:Number(this.countInput.value),wind:this.windInput.checked,rotation:this.rotationInput.checked}}setupSnowflakes(){this.config=this.snowflakeConfig}static get styles(){return _`
+      fieldset {
+        margin-top: 16px;
+      }
+    `}};r([g()],l.prototype,"config",2);r([u("#count")],l.prototype,"countInput",2);r([u("#wind")],l.prototype,"windInput",2);r([u("#rotation")],l.prototype,"rotationInput",2);r([u("#color")],l.prototype,"colorInput",2);l=r([m("ia-snow-story")],l);export{l as IASnowStory};
