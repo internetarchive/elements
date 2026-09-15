123c5d107cbcd457e79589407861faf863459917
jbuckner <jbuckner@users.noreply.github.com>
2026-09-15 00:20:51 +0000

    Deploy preview for PR 102 🛫

---
+++ pr/pr-102/assets/ia-playback-controls-D8hQfc5h.js
@@ -0,0 +1,334 @@
+import{w as e,n as v,t as y,i as w,b as k,a as f}from"./index-CQYwYkKO.js";import{m as o,s as b}from"./runtime-CCgtQBty.js";function P(t,r){return new Intl.NumberFormat(r).format(t)}const s={playing:"playing",paused:"paused"},x=e`
+<svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" viewBox="0 0 18 20"
+  aria-hidden="true"
+>
+  <g fill="none" fill-rule="evenodd" transform="translate(1)">
+    <polygon fill="currentColor" points="7 3 16 17 -2 17" transform="rotate(90 7 10)" />
+    <line x1="15" x2="15" y1="20" stroke="currentColor" stroke-width="2" />
+  </g>
+</svg>
+`,B=e`
+<svg height="60" viewBox="0 0 60 60" width="60" xmlns="http://www.w3.org/2000/svg"
+  aria-hidden="true"
+>
+  <path
+    d="m32 18h6v25h-6zm-10 0h6v25h-6z"
+    fill="currentColor"
+    fill-rule="evenodd"
+  />
+</svg>
+`,C=e`
+<svg height="60" viewBox="0 0 60 60" width="60" xmlns="http://www.w3.org/2000/svg"
+  aria-hidden="true"
+>
+  <path
+    d="m34.5 18 12.5 25h-25z"
+    fill="currentColor"
+    fill-rule="evenodd"
+    transform="matrix(0 1 -1 0 65 -4)"
+  />
+</svg>
+`,M=e`
+<svg xmlns="http://www.w3.org/2000/svg" width="23" height="17" viewBox="0 0 23 17"
+  aria-hidden="true"
+>
+  <g fill="none" fill-rule="evenodd" stroke="currentColor" transform="translate(1 1)">
+    <g stroke-linecap="round" transform="translate(2 2.522)">
+      <path d="M8.5,9.97826087 L15.5,2.97826087" transform="rotate(180 12 6.478)" />
+      <path d="M15.5,11.2695652 L15.5,8.26956522" transform="rotate(90 15.5 9.77)" />
+      <path d="M16.1555266,5.77496471 L16.1480288,4.77499282" transform="rotate(-120 16.152 5.275)" />
+      <path d="M12.9236091,2.48093451 L12.9161756,1.48096213" transform="rotate(-150 12.92 1.98)" />
+      <path d="M8.5,3.47826087 L8.5,0.47826087" transform="rotate(180 8.5 1.978)" />
+      <path d="M4.07639089,2.48093451 L4.08382438,1.48096213" transform="rotate(150 4.08 1.98)" />
+      <path d="M0.844473378,5.77496471 L0.851971223,4.77499282" transform="rotate(120 .848 5.275)" />
+      <path d="M1.5,11.2695652 L1.5,8.26956522" transform="rotate(90 1.5 9.77)" />
+    </g>
+    <path
+      stroke-width="2"
+      d="M10.5,14.9867607 C22.0227273,14.9867607 20.9752066,15.5091468 20.9752066,9.99117383 C20.9752066,4.47320089 16.2852969,0 10.5,0 C4.71470314,0 0.0247933884,4.47320089 0.0247933884,9.99117383 C0.0247933884,15.5091468 -1.02272727,14.9867607 10.5,14.9867607 Z"
+    />
+  </g>
+</svg>
+`,$=e`
+<svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" viewBox="0 0 18 20"
+  aria-hidden="true"
+>
+  <g fill="none" fill-rule="evenodd" transform="translate(1 1)">
+    <polygon fill="currentColor" points="9 2 18 16 0 16" transform="rotate(-90 9 9)" />
+    <line x1=".5" x2=".5" y1="18" stroke="currentColor" stroke-width="2" />
+  </g>
+</svg>
+`,E=e`
+<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
+  aria-hidden="true"
+>
+  <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    <polyline
+      stroke="currentColor"
+      stroke-width="2"
+      points="14.4444444 16.6666667 20 16.6666667 20 3.33333333 5.55555556 3.33333333"
+    />
+    <polygon fill="currentColor" points="5.55555556 0 5.55555556 6.66666667 1.11111111 3.33333333" />
+    <text
+      font-family="HelveticaNeue, Helvetica Neue"
+      font-size="10"
+      font-weight="normal"
+      fill="currentColor"
+    >
+      <tspan x="0" y="17.333">10</tspan>
+    </text>
+  </g>
+</svg>
+`,R=e`
+<svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"
+  aria-hidden="true"
+>
+  <g fill="none" fill-rule="evenodd" transform="matrix(-1 0 0 1 20 0)">
+    <path
+      d="m14.4444444 16.6666667h5.5555556v-13.33333337h-14.44444444"
+      stroke="currentColor"
+      stroke-width="2"
+    />
+    <g fill="currentColor">
+      <path d="m5.55555556 0v6.66666667l-4.44444445-3.33333334z" />
+      <text
+        font-family="HelveticaNeue, Helvetica Neue"
+        font-size="10"
+        transform="matrix(-1 0 0 1 13.333334 0)"
+      >
+        <tspan x="0" y="17.333333">10</tspan>
+      </text>
+    </g>
+  </g>
+</svg>
+`,L=e`
+<svg height="22" viewBox="0 0 22 22" width="22" xmlns="http://www.w3.org/2000/svg"
+  aria-hidden="true"
+>
+  <g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2">
+    <path
+      d="m14 3c2 2.05128205 3 4.71794872 3 8 0 3.2820513-1 5.9487179-3 8m4-18c2 2.56410256 3 5.8974359 3 10s-1 7.4358974-3 10"
+      stroke-linecap="round"
+    />
+    <path d="m10 3.5-4 3.81h-5v8.38h5l4 3.81z" fill="currentColor" stroke-linejoin="round" />
+  </g>
+</svg>
+`,A=e`
+<svg height="22" viewBox="0 0 22 22" width="22" xmlns="http://www.w3.org/2000/svg"
+  aria-hidden="true"
+>
+  <g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2">
+    <path
+      d="m14 3c2 2.05128205 3 4.71794872 3 8 0 3.2820513-1 5.9487179-3 8"
+      stroke-linecap="round"
+    />
+    <path
+      d="m18 1c2 2.56410256 3 5.8974359 3 10s-1 7.4358974-3 10"
+      stroke-linecap="round"
+      stroke-opacity=".1"
+    />
+    <path d="m10 3.5-4 3.81h-5v8.38h5l4 3.81z" fill="currentColor" stroke-linejoin="round" />
+  </g>
+</svg>
+`,I=e`
+<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22"
+  aria-hidden="true"
+>
+  <g fill="none" fill-rule="evenodd">
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-width="2"
+      d="M16,7 L16,15"
+      transform="rotate(-45 16 11)"
+    />
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-width="2"
+      d="M16,7 L16,15"
+      transform="rotate(-135 16 11)"
+    />
+    <polygon
+      fill="currentColor"
+      stroke="currentColor"
+      stroke-linejoin="round"
+      stroke-width="2"
+      points="10 3.5 6 7.31 1 7.31 1 15.69 6 15.69 10 19.5"
+    />
+  </g>
+</svg>
+`;var S=Object.defineProperty,_=Object.getOwnPropertyDescriptor,c=(t,r,u,i)=>{for(var n=i>1?void 0:i?_(r,u):r,d=t.length-1,h;d>=0;d--)(h=t[d])&&(n=(i?h(r,u,n):h(n))||n);return i&&n&&S(r,u,n),n};const a={PlaybackRateChange:"playbackRateChange",VolumeChange:"volumeChange",BackButtonPressed:"back-button-pressed",ForwardButtonPressed:"forward-button-pressed",PlayPauseButtonPressed:"play-pause-button-pressed",PrevSectionButtonPressed:"prev-section-button-pressed",NextSectionButtonPressed:"next-section-button-pressed"},m=1,N=.25,p=.5,g=2,j=.25;let l=class extends w{constructor(){super(...arguments),this.playbackMode=s.paused,this.playbackRate=m,this.volume=1}render(){return k`
+      <div class="container">
+        <div class="vertical-button-stack playback-speed">
+          <div class="vertical-button-container">
+            <button
+              id="playback-rate-btn"
+              class="unstyled-button"
+              aria-label=${this.playbackRateLabel}
+              @click=${this.handlePlaybackRateChange}
+            >
+              ${M}
+            </button>
+          </div>
+          <div class="vertical-button-value" aria-hidden="true">
+            ${this.formattedPlaybackRate}x
+          </div>
+        </div>
+
+        <button
+          id="prev-section-btn"
+          class="jump-btn unstyled-button"
+          aria-label=${o("Previous section")}
+          @click=${this.handlePrevSectionButton}
+        >
+          ${$}
+        </button>
+
+        <button
+          id="back-btn"
+          class="jump-btn unstyled-button"
+          aria-label=${o("Skip back ten seconds")}
+          @click=${this.handleBackButton}
+        >
+          ${E}
+        </button>
+
+        <button
+          id="play-pause-btn"
+          aria-label=${this.isPlaying?o("Pause"):o("Play")}
+          @click=${this.handlePlayPauseButton}
+        >
+          ${this.playPauseButtonIcon}
+        </button>
+
+        <button
+          id="forward-btn"
+          class="jump-btn unstyled-button"
+          aria-label=${o("Skip ahead ten seconds")}
+          @click=${this.handleForwardButton}
+        >
+          ${R}
+        </button>
+
+        <button
+          id="next-section-btn"
+          class="jump-btn unstyled-button"
+          aria-label=${o("Next section")}
+          @click=${this.handleNextSectionButton}
+        >
+          ${x}
+        </button>
+
+        <div class="vertical-button-stack volume">
+          <div class="vertical-button-container">
+            <button
+              id="volume-control-btn"
+              class="unstyled-button"
+              aria-label=${this.volumeLabel}
+              @click=${this.handleVolumeChange}
+            >
+              ${this.volumeButtonIcon}
+            </button>
+          </div>
+          <div class="vertical-button-value" aria-hidden="true">
+            ${this.volumePercent}%
+          </div>
+        </div>
+      </div>
+    `}get isPlaying(){return this.playbackMode===s.playing}get clampedVolume(){return Math.min(Math.max(this.volume||0,0),1)}get volumePercent(){return Math.round(this.clampedVolume*100)}get playbackRateLabel(){return o(b`Playback speed, currently ${this.formattedPlaybackRate}`)}get formattedPlaybackRate(){const t=Number.isFinite(this.playbackRate)?this.playbackRate:m;return P(t)}get volumeLabel(){return o(b`Volume, currently ${this.volumePercent} percent`)}get playPauseButtonIcon(){return this.isPlaying?B:C}get volumeButtonIcon(){return this.clampedVolume<=0?I:this.clampedVolume>=1?L:A}handlePlaybackRateChange(){const t=Math.max(this.playbackRate||p,p);this.playbackRate=t>=g?p:Math.min(t+N,g),this.dispatchEvent(new CustomEvent(a.PlaybackRateChange,{detail:{playbackRate:this.playbackRate}}))}handleVolumeChange(){const t=this.clampedVolume;this.volume=t>=1?0:Math.min(t+j,1),this.dispatchEvent(new CustomEvent(a.VolumeChange,{detail:{volume:this.volume}}))}handleBackButton(){this.dispatchEvent(new Event(a.BackButtonPressed))}handleForwardButton(){this.dispatchEvent(new Event(a.ForwardButtonPressed))}handlePrevSectionButton(){this.dispatchEvent(new Event(a.PrevSectionButtonPressed))}handleNextSectionButton(){this.dispatchEvent(new Event(a.NextSectionButtonPressed))}handlePlayPauseButton(){this.playbackMode=this.isPlaying?s.paused:s.playing,this.dispatchEvent(new Event(a.PlayPauseButtonPressed))}static get styles(){return f`
+      :host {
+        --playback-controls-icon-color--: var(
+          --ia-theme-playback-controls-icon-color,
+          #fff
+        );
+        --playback-controls-play-icon-color--: var(
+          --ia-theme-playback-controls-play-icon-color,
+          #333
+        );
+        --playback-controls-play-button-color--: var(
+          --ia-theme-playback-controls-play-button-color,
+          #fff
+        );
+        --playback-controls-play-button-diameter--: var(
+          --ia-theme-playback-controls-play-button-diameter,
+          4rem
+        );
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding-left: 0.25rem;
+        padding-right: 0.25rem;
+      }
+
+      .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        color: var(--playback-controls-icon-color--);
+        width: 100%;
+      }
+
+      .vertical-button-stack {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+      }
+
+      .vertical-button-container {
+        text-align: center;
+      }
+
+      .vertical-button-container button,
+      .vertical-button-container svg {
+        vertical-align: bottom;
+      }
+
+      .vertical-button-value {
+        font-size: 0.7em;
+        line-height: 1.4em;
+        text-align: center;
+      }
+
+      #play-pause-btn {
+        border-radius: 50%;
+        /* Buttons are border-box, so the UA's default padding would eat into
+           the icon's content box and shrink the glyph relative to the circle
+           by a different amount at every diameter. */
+        padding: 0;
+        height: var(--playback-controls-play-button-diameter--);
+        width: var(--playback-controls-play-button-diameter--);
+        border: none;
+        background-color: var(--playback-controls-play-button-color--);
+        color: var(--playback-controls-play-icon-color--);
+        vertical-align: middle;
+      }
+
+      #play-pause-btn:active {
+        opacity: 0.75;
+      }
+
+      #play-pause-btn svg {
+        width: 100%;
+        height: 100%;
+      }
+
+      .unstyled-button {
+        background: none;
+        border: none;
+        margin: 0;
+        padding: 0;
+        /* Buttons don't inherit color from their parent, so the icons'
+           currentColor needs it passed down explicitly. */
+        color: inherit;
+      }
+
+      button {
+        cursor: pointer;
+      }
+
+      .jump-btn:active {
+        opacity: 0.75;
+      }
+    `}};c([v({type:String})],l.prototype,"playbackMode",2);c([v({type:Number})],l.prototype,"playbackRate",2);c([v({type:Number})],l.prototype,"volume",2);l=c([y("ia-playback-controls")],l);export{s as P};
---
+++ pr/pr-102/assets/ia-scrubber-bar-nZTXEyll.js
@@ -0,0 +1,287 @@
+import{w as m,n as o,t as v,i as g,b as d,a as f,r as w}from"./index-CQYwYkKO.js";import{m as y}from"./runtime-CCgtQBty.js";const b={left:"left",right:"right",neither:"neither"},x=m`
+<svg height="10" viewBox="0 0 8 10" width="8" xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="m4 1 5 8h-10z"
+    fill="currentColor"
+    fill-rule="evenodd"
+    transform="matrix(0 1 -1 0 9 1)"
+  />
+</svg>
+`,$=m`
+<svg height="10" viewBox="0 0 8 10" width="8" xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="m4 1 5 8h-10z"
+    fill="currentColor"
+    fill-rule="evenodd"
+    transform="matrix(0 -1 1 0 -1 9)"
+  />
+</svg>
+`;var S=Object.defineProperty,I=Object.getOwnPropertyDescriptor,k=(e,r,n,i)=>{for(var t=i>1?void 0:i?I(r,n):r,h=e.length-1,c;h>=0;h--)(c=e[h])&&(t=(i?c(r,n,t):c(t))||t);return i&&t&&S(r,n,t),t};let l=class extends g{constructor(){super(...arguments),this.markerMode=b.neither}render(){return d`
+      <div class="container mode-${this.markerMode}">
+        <div class="left-arrow arrow">${x}</div>
+        <div class="center-divider"></div>
+        <div class="right-arrow arrow">${$}</div>
+      </div>
+    `}static get styles(){return f`
+      :host {
+        --section-marker-color--: var(--ia-theme-scrubber-marker-color, #fff);
+        /*
+          Relative to the marker, which the scrubber sizes to the track, so a
+          collapsed divider is exactly as tall as the track it sits on and an
+          expanded one stands proportionally above it. A fixed pixel height
+          here only lines up at one track height.
+        */
+        --section-marker-height-collapsed--: var(
+          --ia-theme-scrubber-marker-height-collapsed,
+          100%
+        );
+        --section-marker-height-expanded--: var(
+          --ia-theme-scrubber-marker-height-expanded,
+          250%
+        );
+        --section-marker-animation-speed--: 0.1s;
+
+        color: var(--section-marker-color--);
+      }
+
+      .container {
+        display: flex;
+        justify-content: center;
+        height: 100%;
+      }
+
+      .arrow {
+        padding-top: 10px;
+        opacity: 1;
+        /*
+          The arrows are hidden in every mode. The mode rules below vary only
+          opacity, which cannot reveal a hidden element, so nothing here brings
+          them back. Reveal them by dropping this line, not by touching the
+          mode rules.
+        */
+        visibility: hidden;
+        transition:
+          opacity var(--section-marker-animation-speed--) ease-out,
+          padding-top var(--section-marker-animation-speed--) ease-out;
+      }
+
+      .container.mode-left .right-arrow {
+        opacity: 0;
+      }
+
+      .container.mode-right .left-arrow {
+        opacity: 0;
+      }
+
+      .container.mode-neither .left-arrow,
+      .container.mode-neither .right-arrow {
+        opacity: 0;
+        padding-top: 75%;
+      }
+
+      .container.mode-neither .center-divider {
+        height: var(--section-marker-height-collapsed--);
+      }
+
+      .center-divider {
+        border-left: 1px solid currentColor;
+        width: 1px;
+        left: 50%;
+        height: var(--section-marker-height-expanded--);
+        align-self: flex-end;
+        transition: height var(--section-marker-animation-speed--) ease-out;
+      }
+    `}};k([o({type:String})],l.prototype,"markerMode",2);l=k([v("ia-section-marker")],l);var M=Object.defineProperty,E=Object.getOwnPropertyDescriptor,s=(e,r,n,i)=>{for(var t=i>1?void 0:i?E(r,n):r,h=e.length-1,c;h>=0;h--)(c=e[h])&&(t=(i?c(r,n,t):c(t))||t);return i&&t&&M(r,n,t),t};const u={ValueChange:"valuechange",UserInteractionStarted:"userInteractionStarted",UserInteractionEnded:"userInteractionEnded"},p=new Set(["ArrowLeft","ArrowRight","ArrowUp","ArrowDown","Home","End","PageUp","PageDown"]);let a=class extends g{constructor(){super(...arguments),this.value=0,this.min=0,this.max=100,this.step=.1,this.sectionMarkerPercentages=[],this.expandSectionMarkers=!1,this.label=y("Playback position"),this.currentValue=0,this.userIsInteracting=!1}get percentage(){const e=this.max-this.min;return e===0?0:(this.currentValue-this.min)/e*100}render(){const e=this.surroundingMarkers;return d`
+      <div class="container" style="--fill-percent--: ${this.percentage}%">
+        <div class="color-fill"></div>
+
+        <div class="marker-container">
+          ${this.sectionMarkerPercentages.map(r=>d`
+              <ia-section-marker
+                data-location=${r}
+                style="left: ${r}%"
+                .markerMode=${this.markerModeFor(r,e)}
+              ></ia-section-marker>
+            `)}
+        </div>
+
+        <input
+          id="slider"
+          type="range"
+          aria-label=${this.label}
+          min=${this.min}
+          max=${this.max}
+          step=${this.step}
+          .value=${String(this.currentValue)}
+          @mousedown=${this.interactionStarted}
+          @mouseup=${this.interactionEnded}
+          @touchstart=${this.interactionStarted}
+          @touchend=${this.interactionEnded}
+          @touchcancel=${this.interactionEnded}
+          @keydown=${this.handleKeyDown}
+          @keyup=${this.handleKeyUp}
+          @blur=${this.handleBlur}
+          @input=${this.handleSlide}
+          @change=${this.handleSlide}
+        />
+      </div>
+    `}updated(e){this.userIsInteracting||!e.has("value")||(this.currentValue=this.value)}get surroundingMarkers(){if(!this.expandSectionMarkers)return{};const e=this.sortedMarkers;return{lower:e.filter(r=>r<=this.currentValue).pop(),upper:e.find(r=>r>this.currentValue)}}get sortedMarkers(){return[...this.sectionMarkerPercentages].sort((e,r)=>e-r)}markerModeFor(e,r){return e===r.upper?b.left:e===r.lower?b.right:b.neither}handleSlide(e){this.currentValue=parseFloat(e.target.value),this.dispatchEvent(new CustomEvent(u.ValueChange,{detail:{value:this.currentValue}}))}interactionStarted(){this.userIsInteracting=!0,this.dispatchEvent(new Event(u.UserInteractionStarted))}handleKeyDown(e){!p.has(e.key)||this.userIsInteracting||this.interactionStarted()}handleKeyUp(e){p.has(e.key)&&this.interactionEnded()}handleBlur(){this.userIsInteracting&&this.interactionEnded()}interactionEnded(){this.userIsInteracting=!1,this.dispatchEvent(new Event(u.UserInteractionEnded))}static get styles(){return f`
+      :host {
+        --scrubber-bar-height--: var(--ia-theme-scrubber-bar-height, 20px);
+        --scrubber-marker-inset--: var(--ia-theme-scrubber-marker-inset, 10px);
+
+        --scrubber-thumb-color--: var(--ia-theme-scrubber-thumb-color, #fff);
+        --scrubber-thumb-diameter--: var(
+          --ia-theme-scrubber-thumb-diameter,
+          20px
+        );
+        --scrubber-thumb-border--: var(
+          --ia-theme-scrubber-thumb-border,
+          1px solid #000
+        );
+        --scrubber-thumb-border-radius--: var(
+          --ia-theme-scrubber-thumb-border-radius,
+          50%
+        );
+        /*
+          Centres the thumb on the track. The thumb is content-box, so its
+          border adds 1px above and below the diameter, hence the extra -1px.
+          Comes out at -6px for the default track and thumb sizes.
+        */
+        --scrubber-thumb-top-margin--: var(
+          --ia-theme-scrubber-thumb-top-margin,
+          calc(
+            (
+                var(--scrubber-track-height--) - var(
+                    --scrubber-thumb-diameter--
+                  )
+              ) /
+              2 - 1px
+          )
+        );
+
+        --scrubber-track-height--: var(--ia-theme-scrubber-track-height, 10px);
+        --scrubber-track-border--: var(
+          --ia-theme-scrubber-track-border,
+          1px solid #fff
+        );
+        --scrubber-track-border-radius--: var(
+          --ia-theme-scrubber-track-border-radius,
+          5px
+        );
+        --scrubber-track-color--: var(
+          --ia-theme-scrubber-track-color,
+          rgba(0, 0, 0, 0.1)
+        );
+        --scrubber-track-fill-color--: var(
+          --ia-theme-scrubber-track-fill-color,
+          #3272b6
+        );
+
+        /*
+          How far off the bottom the track sits. The fill and the section
+          markers both line up against this, so they follow the track when its
+          height is themed. Comes out at 7px for the default sizes.
+        */
+        --scrubber-track-offset--: calc(
+          (var(--scrubber-bar-height--) - var(--scrubber-track-height--)) / 2 +
+            2px
+        );
+      }
+
+      .container {
+        position: relative;
+        height: var(--scrubber-bar-height--);
+      }
+
+      /*
+        The fill is a plain element rather than a track pseudo-element, so the
+        played portion is just a hard gradient stop driven by a custom property.
+      */
+      .color-fill {
+        height: var(--scrubber-track-height--);
+        border-radius: 1em;
+        position: absolute;
+        bottom: var(--scrubber-track-offset--);
+        left: 2px;
+        right: -2px;
+        background: linear-gradient(
+          to right,
+          var(--scrubber-track-fill-color--) 0%,
+          var(--scrubber-track-fill-color--) var(--fill-percent--),
+          var(--scrubber-track-color--) var(--fill-percent--),
+          var(--scrubber-track-color--) 100%
+        );
+      }
+
+      .marker-container {
+        position: absolute;
+        left: var(--scrubber-marker-inset--);
+        right: var(--scrubber-marker-inset--);
+        height: 100%;
+      }
+
+      ia-section-marker {
+        position: absolute;
+        width: 2rem;
+        height: var(--scrubber-track-height--);
+        bottom: var(--scrubber-track-offset--);
+        /*
+          The left offset puts the marker where it belongs, but the divider runs
+          down the middle of the marker, so it shifts back by half its own width
+          to line the divider up with that point.
+        */
+        transform: translateX(-50%);
+      }
+
+      input[type='range'] {
+        -webkit-appearance: none;
+        appearance: none;
+        height: var(--scrubber-bar-height--);
+        padding: 0;
+        width: 100%;
+        background: none;
+        outline: none;
+        position: absolute;
+        bottom: 0;
+      }
+
+      input[type='range']::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        appearance: none;
+        box-sizing: content-box;
+        margin-top: var(--scrubber-thumb-top-margin--);
+        background-color: var(--scrubber-thumb-color--);
+        height: var(--scrubber-thumb-diameter--);
+        width: var(--scrubber-thumb-diameter--);
+        border-radius: var(--scrubber-thumb-border-radius--);
+        border: var(--scrubber-thumb-border--);
+        cursor: pointer;
+      }
+
+      input[type='range']::-moz-range-thumb {
+        background-color: var(--scrubber-thumb-color--);
+        height: var(--scrubber-thumb-diameter--);
+        width: var(--scrubber-thumb-diameter--);
+        border-radius: var(--scrubber-thumb-border-radius--);
+        border: var(--scrubber-thumb-border--);
+        cursor: pointer;
+      }
+
+      input[type='range']::-webkit-slider-runnable-track {
+        border: var(--scrubber-track-border--);
+        height: var(--scrubber-track-height--);
+        border-radius: var(--scrubber-track-border-radius--);
+      }
+
+      input[type='range']::-moz-range-track {
+        border: var(--scrubber-track-border--);
+        height: var(--scrubber-track-height--);
+        border-radius: var(--scrubber-track-border-radius--);
+      }
+
+      input[type='range']::-moz-range-progress {
+        height: var(--scrubber-track-height--);
+        border-radius: var(--scrubber-track-border-radius--);
+      }
+    `}};s([o({type:Number})],a.prototype,"value",2);s([o({type:Number})],a.prototype,"min",2);s([o({type:Number})],a.prototype,"max",2);s([o({type:Number})],a.prototype,"step",2);s([o({type:Array})],a.prototype,"sectionMarkerPercentages",2);s([o({type:Boolean})],a.prototype,"expandSectionMarkers",2);s([o({type:String})],a.prototype,"label",2);s([w()],a.prototype,"currentValue",2);a=s([v("ia-scrubber-bar")],a);
